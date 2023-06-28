dbfa8b69375f5af8e44f73487d6aba8b7259fbff
Chmiel <achmiel@beckettcorp.com>
2023-06-28 13:33:57 -0400

    Removing duplicate files

--- VISolver/Domain 2.py
+++
@@ -1,7 +0,0 @@
-
-class Domain(object):
-
-    def __init__(self):
-        raise NotImplementedError(
-            'This is a generic domain object. '
-            'You need to pick a specific domain to use.')
